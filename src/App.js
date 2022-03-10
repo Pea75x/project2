7c50b52dac6757a8662fd11d0da79181b009ec00
Priya <priya_93_x@hotmail.co.uk>
2022-03-10 12:41:04 +0000

    Added game componenent

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Hat from './components/Hat';
 import MyHouse from './components/MyHouse';
 import Classmates from './components/Classmates';
+import Game from './components/Game';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Hat />} />
-        <Route path="/myhouse" element={<MyHouse />} />
-        <Route path="/classmates" element={<Classmates />} />
+        <Route path='/' element={<Hat />} />
+        <Route path='/myhouse' element={<MyHouse />} />
+        <Route path='/classmates' element={<Classmates />} />
+        <Route path='/game' element={<Game />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Game.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import axios from 'axios';
+const currentHouse = localStorage.getItem('myHouse');
+
+function Game() {
+  const endPoint = 'http://hp-api.herokuapp.com/api/characters';
+  const [potterData, setPotterData] = React.useState(null);
+  const [randomPerson, setRandomPerson] = React.useState(undefined);
+  const [points, setPoints] = React.useState(0);
+  const [goes, setGoes] = React.useState(0);
+  const [pointsText, setPointsText] = React.useState('');
+
+  const hpApi = () => {
+    return axios.get(`${endPoint}`);
+  };
+
+  function getNextCard() {
+    setRandomPerson(Math.floor(Math.random() * potterData.length));
+    setGoes(goes + 1);
+    console.log(randomPerson);
+  }
+
+  function checkAnswer(event) {
+    const yourAnswer = event.target.innerText;
+    const correctAnswer = potterData[randomPerson].house;
+    if (yourAnswer === correctAnswer) {
+      setPoints(points + 10);
+      setPointsText(`10 points to ${currentHouse}!`);
+      getNextCard();
+    } else {
+      setPointsText(
+        `5 points will be taken from ${currentHouse} for your serious lack of judgement...`
+      );
+      getNextCard();
+    }
+  }
+
+  React.useEffect(() => {
+    const getData = async () => {
+      try {
+        const { data } = await hpApi();
+        setPotterData(
+          data.filter((person) => person.image !== '' && person.house !== '')
+        );
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getData();
+  }, []);
+
+  console.log(potterData);
+  console.log('goes:', goes);
+
+  if (goes < 10) {
+    return (
+      <>
+        <div className='gameBackground'>
+          <div className='game-container'>
+            <h1>Whos House is whos?</h1>
+            <div className='pointsText'>{pointsText}</div>
+            {randomPerson === undefined ? (
+              <button onClick={getNextCard} className='playGame game-button'>
+                Play Game
+              </button>
+            ) : (
+              <div className='gameStart'>
+                <div className='game-card'>
+                  <h2>{potterData[randomPerson].name}</h2>
+                  <img
+                    src={potterData[randomPerson].image}
+                    alt={potterData[randomPerson].name}
+                  />
+                </div>
+                <div className='houseButtons'>
+                  <button className='game-button' onClick={checkAnswer}>
+                    Gryffindor
+                  </button>
+                  <button className='game-button' onClick={checkAnswer}>
+                    Hufflepuff
+                  </button>
+                  <button className='game-button' onClick={checkAnswer}>
+                    Ravenclaw
+                  </button>
+                  <button className='game-button' onClick={checkAnswer}>
+                    Slytherin
+                  </button>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div>
+          <h1>Game Over!</h1>
+          <p>{points} points</p>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Game;
